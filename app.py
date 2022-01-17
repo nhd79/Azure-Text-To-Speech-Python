9fccf247830dafbe20869ef54c837094403c3901
DuyNH <hoangduy791999@gmail.com>
2022-01-17 12:15:00 +0700

    encountering caching problem

--- app.py
+++ app.py
@@ -1,24 +1,52 @@
-from flask import Flask, redirect, url_for, request, render_template
+import json
+from azure.cognitiveservices.speech.audio import AudioOutputConfig
+import azure.cognitiveservices.speech as speechsdk
+from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
+
+
+def textToSpeech(ttstext, languageselect, voiceselect, voicestyleselect):
+    config_file_name = "config_file_dev.json"
+
+    with open(config_file_name, 'r') as json_data_file:
+        configuration = json.load(json_data_file)
+
+    # Speech SDK
+    speech_key = configuration["speech_api"]["speech_key"]
+    service_region = configuration["speech_api"]["service_region"]
+
+    speech_config = speechsdk.SpeechConfig(
+        subscription=speech_key, region=service_region)
+
+    speech_config.speech_synthesis_language = languageselect
+
+    speech_config.speech_synthesis_voice_name = voiceselect
+
+    audio_config = AudioOutputConfig(filename="static\\result.wav")
+
+    speech_synthesizer = speechsdk.SpeechSynthesizer(
+        speech_config=speech_config, audio_config=audio_config)
+
+    text = ttstext
+
+    result = speech_synthesizer.speak_text_async(text).get()
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-# @app.route('/success/<text><language>')
-# def success(text):
-#    return 'welcome %s' % text + 'sdhfbsd'
 
-@app.route('/convert',methods = ['POST'])
+@app.route('/convert', methods=['POST'])
 def convert():
     if request.method == 'POST':
         ttstext = request.form.get('ttstext')
         languageselect = request.form.get('languageselect')
         voiceselect = request.form.get('voiceselect')
         voicestyleselect = request.form.get('voicestyleselect')
-        #   return redirect(url_for('',text = ttstext))
-        return render_template('index.html', ttstext=ttstext,languageselect=languageselect,voiceselect=voiceselect,voicestyleselect=voicestyleselect)
+        textToSpeech(ttstext, languageselect, voiceselect, voicestyleselect)
+        return render_template('index.html', audio="1")
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
