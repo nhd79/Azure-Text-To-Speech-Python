08fa95cda10c1a1d89bd392d1ad0cb6d55687f27
DuyNH <hoangduy791999@gmail.com>
2022-01-17 17:07:01 +0700

    still stuck

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import json
+import os
 from azure.cognitiveservices.speech.audio import AudioOutputConfig
 import azure.cognitiveservices.speech as speechsdk
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 app = Flask(__name__)
+used = 0
 
 
 def textToSpeech(ttstext, languageselect, voiceselect, voicestyleselect):
@@ -34,18 +36,25 @@ def textToSpeech(ttstext, languageselect, voiceselect, voicestyleselect):
 
 @app.route("/")
 def index():
+    if used == 1:
+        return render_template('index.html', audio="1")
     return render_template('index.html')
 
 
 @app.route('/convert', methods=['POST'])
 def convert():
     if request.method == 'POST':
+        global used
+        used = 1
+
         ttstext = request.form.get('ttstext')
         languageselect = request.form.get('languageselect')
         voiceselect = request.form.get('voiceselect')
         voicestyleselect = request.form.get('voicestyleselect')
+
         textToSpeech(ttstext, languageselect, voiceselect, voicestyleselect)
-        return render_template('index.html', audio="1")
+        # return render_template('index.html', audio="1")
+        return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
